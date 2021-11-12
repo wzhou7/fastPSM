7c2eb3bfa57c48bb5d656fd1de10da3107e2d952
Zhou <wzhou4@utk.edu>
2021-11-12 12:16:44 -0500

    nothing

--- R/wzhou4_logit_default.R
+++ R/wzhou4_logit_default.R
@@ -1,6 +1,6 @@
 wzhou4_logit_default <- function(data_T1, id="AppID", target="Updated",
-                predictors=c("Subcategory_new", "IsFree", "DaysSinceUpdate"),
-                report="report_file_name"){
+                                 predictors=c("Subcategory_new", "IsFree", "DaysSinceUpdate"),
+                                 report="report_file_name"){
     
     scores <- "this is a dummy from wzhou4_logit_default"
     
