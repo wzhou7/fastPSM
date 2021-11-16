598170f6aab898f86447cf815e8e3dba684e7bc8
wzhou7 <wzhou7@gmail.com>
2021-11-16 15:59:18 -0500

    Update mz_automl.R

--- R/mz_automl.R
+++ R/mz_automl.R
@@ -2,7 +2,41 @@ mz_automl <- function(data_T1, id="AppID", target="Updated",
                                  predictors=c("Subcategory_new", "IsFree", "DaysSinceUpdate"),
                                  report="report_file_name"){
     
-    scores <- "this is a dummy from mz_automl"
+    # initialize scored as all missing
+    scores <- rep(NA,NROW(data_T1))
+  
+    # fit automl with default parameters
+  
+    # start h2o cluster
+    invisible(h2o.init())
+  
+  # convert data as h2o type
+  train_h = as.h2o(train)
+  test_h = as.h2o(test)
+  
+  # set label type
+  y = 'Update'
+  pred = setdiff(names(train), y)
+  
+  #convert variables to factors
+  train[,y] = as.factor(train[,y])
+  test[,y] = as.factor(test[,y])
+  
+  # Run AutoML for 20 base models
+  aml = h2o.automl(x = pred, y = y,
+                   training_frame = train_h,
+                   max_models = 20,
+                   seed = 1,
+                   max_runtime_secs = 20
+  )
+  
+  # AutoML Leaderboard
+  lb = aml@leaderboard
+  
+  # prediction result on test data
+  prediction = h2o.predict(aml@leader, test_h[,-5]) %>%
+    as.data.frame()
+  
     
     # the return value should include [AppID, Updated, PropensityScore] 
     # for each app in the input dataframe
