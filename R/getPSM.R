56a8c2ee9a2dcc9ee6edc67e85d85adcd99e2e0f
wzhou7 <wzhou4@utk.edu>
2022-03-05 21:40:26 -0500

    Update getPSM.R

--- R/getPSM.R
+++ R/getPSM.R
@@ -1,21 +1,40 @@
 #' @export
 getPSM <- function(formula, data, method=c("logit")){
 
+    scores <- NULL
+    
     if("logit" %in% method){
-        scores <- PSM_logit(formula, data)
+        logit_score <- PSM_logit(formula, data)
+        scores <- cbind(scores, logit_score)
     }
 
     if("rpart" %in% method){
-        scores <- PSM_rpart(formula, data)
+        rpart_score <- PSM_rpart(formula, data)
+        scores <- cbind(scores, rpart_score)
     }
 
     if("svm" %in% method){
-        scores <- PSM_svm(formula, data)
+        svm_score <- PSM_svm(formula, data)
+        scores <- cbind(scores, svm_score)
     }
 
+    if("nb" %in% method){
+        nb_score <- PSM_nb(formula, data)
+        scores <- cbind(scores, nb_score)
+    }
+
+    if("ann" %in% method){
+        ann_score <- PSM_ann(formula, data)
+        scores <- cbind(scores, ann_score)
+    }
+    
     if("automl" %in% method){
-        scores <- PSM_automl(formula, data)
+        automl_score <- PSM_automl(formula, data)
+        scores <- cbind(scores, automl_score)
     }
 
+    # re-arrange the output columns
+    scores <- as.data.frame(scores)
+    scores <- scores[,method]
     return(scores)
 }
